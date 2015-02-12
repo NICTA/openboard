018dcaaf199221401a87f379fe1a6f31232271ab
Paul Haesler <paul.haesler@nicta.com.au>
2015-02-12 12:10:48 +1100

    Migration cleanup 4: wipe and start again!

--- dashboard_api/widget_data/migrations/0001_initial.py
+++ dashboard_api/widget_data/migrations/0001_initial.py
@@ -2,14 +2,12 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import datetime
-from django.utils.timezone import utc
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('widget_def', '0001_squashed_0013_auto_20150211_0940'),
+        ('widget_def', '0001_initial'),
     ]
 
     operations = [
@@ -17,15 +15,15 @@ class Migration(migrations.Migration):
             name='StatisticData',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('label', models.CharField(max_length=80, null=True, blank=True)),
                 ('intval', models.IntegerField(null=True, blank=True)),
                 ('decval', models.DecimalField(null=True, max_digits=10, decimal_places=4, blank=True)),
                 ('strval', models.CharField(max_length=400, null=True, blank=True)),
                 ('trend', models.SmallIntegerField(blank=True, null=True, choices=[(1, b'Upwards'), (0, b'Steady'), (-1, b'Downwards')])),
+                ('last_updated', models.DateTimeField(auto_now=True)),
+                ('icon_code', models.ForeignKey(blank=True, to='widget_def.IconCode', null=True)),
                 ('statistic', models.ForeignKey(to='widget_def.Statistic', unique=True)),
                 ('traffic_light_code', models.ForeignKey(blank=True, to='widget_def.TrafficLightScaleCode', null=True)),
-                ('last_updated', models.DateTimeField(default=datetime.datetime(2015, 2, 4, 3, 22, 13, 334267, tzinfo=utc), auto_now=True)),
-                ('icon_code', models.ForeignKey(blank=True, to='widget_def.IconCode', null=True)),
-                ('label', models.CharField(max_length=80, null=True, blank=True)),
             ],
             options={
             },
@@ -42,9 +40,9 @@ class Migration(migrations.Migration):
                 ('trend', models.SmallIntegerField(blank=True, null=True, choices=[(1, b'Upwards'), (0, b'Steady'), (-1, b'Downwards')])),
                 ('sort_order', models.IntegerField()),
                 ('last_updated', models.DateTimeField(auto_now=True)),
+                ('icon_code', models.ForeignKey(blank=True, to='widget_def.IconCode', null=True)),
                 ('statistic', models.ForeignKey(to='widget_def.Statistic')),
                 ('traffic_light_code', models.ForeignKey(blank=True, to='widget_def.TrafficLightScaleCode', null=True)),
-                ('icon_code', models.ForeignKey(blank=True, to='widget_def.IconCode', null=True)),
             ],
             options={
                 'ordering': ('statistic', 'sort_order'),
