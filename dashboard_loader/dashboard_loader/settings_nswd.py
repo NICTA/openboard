d3d235ce334dc1177dd8a81fc48291adebd43d45
Paul Haesler <paul.haesler@nicta.com.au>
2015-02-13 14:49:17 +1100

    Fixed static url for "prod"

--- dashboard_loader/dashboard_loader/settings_nswd.py
+++ dashboard_loader/dashboard_loader/settings_nswd.py
@@ -96,5 +96,5 @@ STATICFILES_FINDERS = (
         'django.contrib.staticfiles.finders.FileSystemFinder',
         'django.contrib.staticfiles.finders.AppDirectoriesFinder',
         )
-STATIC_URL = '/update/static/'
+STATIC_URL = '/static/'
 
