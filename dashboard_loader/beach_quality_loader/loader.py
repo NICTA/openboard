a49a2156761e7c19cefe040c77637c81d7ae5954
Paul Haesler <paul.haesler@nicta.com.au>
2015-03-11 10:45:29 +1100

    Beach loader collecting stats - no widgets defined yet.

--- dashboard_loader/beach_quality_loader/loader.py
+++ dashboard_loader/beach_quality_loader/loader.py
@@ -7,23 +7,103 @@ import xml.etree.ElementTree as ET
 
 from dashboard_loader.loader_utils import LoaderException, set_statistic_data, clear_statistic_data,get_icon, get_statistic, clear_statistic_list, add_statistic_list_item, call_in_transaction
 
+from beach_quality_loader.models import BeachSummaryHistory, CurrentBeachRating
+
 # Refresh data every 3 hours
 refresh_rate = 60*60*3
 
 def update_data(loader, verbosity=0):
     messages = []
     http = httplib.HTTPConnection("www.environment.nsw.gov.au")
-    http.request("GET", "http://www.environment.nsw.gov.au/beachapp/OceanBulletin.xml")
-    resp = http.getresponse()
-    process_xml("Oceans", resp)
-    http.request("GET", "http://www.environment.nsw.gov.au/beachapp/SydneyBulletin.xml")
-    resp = http.getresponse()
-    process_xml("Sydney", resp)
+    try:
+        http.request("GET", "http://www.environment.nsw.gov.au/beachapp/OceanBulletin.xml")
+        resp = http.getresponse()
+        messages.extend(call_in_transaction(process_xml,BeachSummaryHistory.SYDNEY_OCEAN, resp))
+    except LoaderException, e:
+        messages.append("Error updating Sydney Ocean beach data: %s" % unicode(e))
+    try:
+        http.request("GET", "http://www.environment.nsw.gov.au/beachapp/SydneyBulletin.xml")
+        resp = http.getresponse()
+        messages.extend(call_in_transaction(process_xml,BeachSummaryHistory.SYDNEY_HARBOUR, resp))
+    except LoaderException, e:
+        messages.append("Error updating Sydney Ocean beach data: %s" % unicode(e))
+    try:
+        http.request("GET", "http://www.environment.nsw.gov.au/beachapp/BotanyBulletin.xml")
+        resp = http.getresponse()
+        messages.extend(call_in_transaction(process_xml,BeachSummaryHistory.BOTANY_BAY, resp))
+    except LoaderException, e:
+        messages.append("Error updating Botany Bay beach data: %s" % unicode(e))
+    try:
+        http.request("GET", "http://www.environment.nsw.gov.au/beachapp/PittwaterBulletin.xml")
+        resp = http.getresponse()
+        messages.extend(call_in_transaction(process_xml,BeachSummaryHistory.PITTWATER, resp))
+    except LoaderException, e:
+        messages.append("Error updating Pittwater beach data: %s" % unicode(e))
+    try:
+        http.request("GET", "http://www.environment.nsw.gov.au/beachapp/CentralCoastBulletin.xml")
+        resp = http.getresponse()
+        messages.extend(call_in_transaction(process_xml,BeachSummaryHistory.CENTRAL_COAST, resp))
+    except LoaderException, e:
+        messages.append("Error updating Central Coast beach data: %s" % unicode(e))
+    try:
+        http.request("GET", "http://www.environment.nsw.gov.au/beachapp/IllawarraBulletin.xml")
+        resp = http.getresponse()
+        messages.extend(call_in_transaction(process_xml,BeachSummaryHistory.ILLAWARRA, resp))
+    except LoaderException, e:
+        messages.append("Error updating Illawarra beach data: %s" % unicode(e))
     http.close()
     return messages
 
-def process_xml(title, resp):
+def process_xml(region, resp):
     xml = ET.parse(resp)
+    title = BeachSummaryHistory.regions[region]
+    # dump_xml(region, xml)
+    num_unlikely = 0
+    num_possible = 0
+    num_likely = 0
+    for elem in xml.getroot()[0]:
+        if elem.tag == 'item':
+            beach_name = None
+            advice = None
+            item = elem
+            for i in item:
+                if i.tag == 'title':
+                    beach_name = i.text
+                elif i.tag.endswith('}data'):
+                    data = i
+                    for d in data:
+                        if d.tag.endswith('}advice'):
+                            advice = d.text.strip()
+            if not beach_name: 
+                raise LoaderException("Parse error: Beach with no name")
+            if not advice:
+                raise LoaderException("Parse error: Beach %s has no advice" % beach_name)
+            try:
+                beach = CurrentBeachRating.objects.get(region=region, beach_name = beach_name)
+            except CurrentBeachRating.DoesNotExist:
+                beach = CurrentBeachRating(region=region, beach_name=beach_name)
+            beach.rating = beach.parse_advice(advice)
+            beach.save()
+            if beach.rating == beach.GOOD:
+                num_unlikely += 1
+            elif beach.rating == beach.FAIR:
+                num_possible += 1
+            else:
+                num_likely += 1
+    try:
+        summary = BeachSummaryHistory.objects.get(region=region, day=datetime.datetime.today())
+    except BeachSummaryHistory.DoesNotExist:
+        summary = BeachSummaryHistory(region=region)
+    if num_unlikely + num_possible + num_likely == 0:
+        raise LoaderException("No beaches found in RSS feed")
+    summary.num_pollution_unlikely = num_unlikely
+    summary.num_pollution_possible = num_possible
+    summary.num_pollution_likely = num_likely
+    summary.save()
+    return ["Loaded data for %s beaches" % title]
+
+def dump_xml(region, xml):
+    title = BeachSummaryHistory.regions[region]
     print "%s:" % title
     for elem in xml.getroot()[0]:
         if elem.tag.endswith( '}data'):
@@ -66,3 +146,4 @@ def process_xml(title, resp):
                             print "long: %s" % d.text  
             print "-------------------------"
     print "======================================" 
+
--- dashboard_loader/beach_quality_loader/models.py
+++ dashboard_loader/beach_quality_loader/models.py
@@ -1,3 +1,78 @@
 from django.db import models
 
 # Create your models here.
+
+class BeachSummaryHistory(models.Model):
+    SYDNEY_OCEAN = 'SYDOC'
+    SYDNEY_HARBOUR = 'SYDHB'
+    BOTANY_BAY = 'BOTNY'
+    PITTWATER = 'PIWAT'
+    CENTRAL_COAST = 'CTRCT'
+    ILLAWARRA = 'ILLAW'
+    regions = {
+        SYDNEY_OCEAN: 'Sydney Ocean',
+        SYDNEY_HARBOUR: 'Sydney Harbour',
+        BOTANY_BAY: 'Botany Bay, Georges River and Port Hacking',
+        PITTWATER: 'Pittwater',
+        CENTRAL_COAST: 'Central Coast',
+        ILLAWARRA: 'Illawarra',
+    }
+    day=models.DateField(auto_now=True)
+    region=models.CharField(max_length=5, choices=regions.items())
+    num_pollution_unlikely=models.IntegerField()
+    num_pollution_possible=models.IntegerField()
+    num_pollution_likely=models.IntegerField()
+    def is_sydney(self):
+        return self.region in (self.SYDNEY_OCEAN,
+                                self.SYDNEY_HARBOUR,
+                                # self.BOTANY_BAY,
+                                # self.PITTWATER,
+        )
+                            
+    def region_display(self):
+        return self.regions[self.region]
+    def __unicode__(self):
+        return "%s %s: +%d %d %d-" % (self.region_display(),
+                                    self.day.strftime("%d/%m/%Y"),
+                                    self.num_pollution_unlikely,
+                                    self.num_pollution_possible,
+                                    self.num_pollution_likely)
+    class Meta:
+        unique_together=(("day", "region"),)
+        ordering = ("day", "region")
+
+class CurrentBeachRating(models.Model):
+    GOOD=1
+    FAIR=2
+    POOR=3
+    ratings = [ "-", 
+                "Pollution is unlikely. Enjoy your swim!",
+                "Pollution is possible. Take care.",
+                "Pollution is likely. Avoid swimming today.",
+    ]
+    region=models.CharField(max_length=5, choices=BeachSummaryHistory.regions.items())
+    beach_name=models.CharField(max_length=100)
+    rating=models.SmallIntegerField(choices=(
+                    (GOOD, ratings[GOOD]),
+                    (FAIR, ratings[FAIR]),
+                    (POOR, ratings[POOR]),
+                ))
+    day_updated=models.DateField(auto_now=True)
+    def region_display(self):
+        return BeachSummaryHistory.regions[self.region]
+    def rating_display(self):
+        return self.ratings[self.rating]
+    def __unicode__(self):
+        return "%s (%s): %s" % (self.beach, 
+                        self.region_display(), self.rating_display())
+    def parse_advice(self, advice):
+        if "unlikely" in advice:
+            return self.GOOD
+        elif "possible" in advice:
+            return self.FAIR
+        else:
+            return self.GOOD
+    class Meta:
+        unique_together=(("region", "beach_name"),)
+        ordering=("region", "beach_name", "-rating")
+
