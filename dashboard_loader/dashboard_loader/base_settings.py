390dc9ba6870ff4e4e6db85ec7094f590ff0e028
Paul Haesler <paul.haesler@nicta.com.au>
2015-03-23 14:49:48 +1100

    Celery timeout fix.

--- dashboard_loader/dashboard_loader/base_settings.py
+++ dashboard_loader/dashboard_loader/base_settings.py
@@ -89,6 +89,10 @@ CELERYBEAT_SCHEDULE = {
     },
 }
 
+# Allow tasks to run for up to 12 hours - Is there a smarter way
+# to load the GTFS static data?
+CELERYD_TASK_TIME_LIMIT = 60 * 60 * 12
+
 LOGIN_URL="/login"
 LOGOUT="/logout"
 
