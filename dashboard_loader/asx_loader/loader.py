eeeb01d6aad726c861e7c3059c318488ae3c7d1a
Paul Haesler <paul.haesler@nicta.com.au>
2015-04-13 13:43:03 +1000

    Stock ticker for ASX widget.

--- dashboard_loader/asx_loader/loader.py
+++ dashboard_loader/asx_loader/loader.py
@@ -4,10 +4,10 @@ from HTMLParser import HTMLParser, HTMLParseError
 
 from django.conf import settings
 
-from dashboard_loader.loader_utils import set_statistic_data, call_in_transaction
+from dashboard_loader.loader_utils import set_statistic_data, call_in_transaction, clear_statistic_list, add_statistic_list_item
 
-# Refresh every 40 seconds
-refresh_rate = 40
+# Refresh every 100 seconds
+refresh_rate = 100
 
 class ASXHtmlParser(HTMLParser):
     def __init__(self, messages, verbosity, *args, **kwargs):
@@ -68,7 +68,7 @@ class ASXHtmlParser(HTMLParser):
                 value = decimal.Decimal(strval)
                 set_statistic_data("asx", "nsw", "rt", "movement", value)  
                 if self.verbosity > 5:
-                    print "RT percent move: %s" % dat
+                    print "RT percent move: %s" % data
                 if self.index_value.is_zero():
                     trend = 0
                 elif self.index_value.is_signed():
@@ -97,10 +97,154 @@ class ASXHtmlParser(HTMLParser):
                     if self.verbosity > 5:
                         print "52wk Max: %s" % data
 
+class StockQuote(object):
+    def __init__(self, code, name=None, last_trade_price=None, percent=None,
+                        is_neg=None):
+        self.code = code
+        self._name = name
+        self.last_trade_price = last_trade_price
+        self.percent = percent
+        self._is_neg = None
+        if is_neg:
+            self._is_neg = True
+    def set_percent(self, percent):
+        self._percent = percent
+        if percent is None:
+            self._is_neg = None
+        elif percent.is_signed() and not percent.is_zero():
+            self._is_neg = True
+        elif self._is_neg and not percent.is_zero():
+            self._percent = self._percent.copy_negate()
+        else:
+            self._is_neg = False
+    def get_percent(self):
+        return self._percent
+    percent = property(get_percent, set_percent)
+    def set_is_neg(self, is_neg):
+        if is_neg:
+            self._is_neg = True
+            if self._percent is not None and not self._percent.is_signed() and not self._percent.is_zero():
+                self._percent = self._percent.copy_negate()
+        else:
+            self._is_neg = False
+            if self._percent is not None and self._percent.is_signed():
+                self._percent = self._percent.copy_negate()
+    def get_is_neg(self):
+        return self._is_neg
+    is_neg = property(get_is_neg, set_is_neg)
+    def set_name(self, name):
+        if self._name:
+            self._name += " " + name
+        else:
+            self._name = name
+    def get_name(self):
+        return self._name
+    name = property(get_name, set_name)
+    def trend(self):
+        if self._percent.is_zero():
+            return 0
+        elif self._is_neg:
+            return -1
+        else:
+            return 1
+
+class ASXQuoteHtmlParser(HTMLParser):
+    def __init__(self, messages, verbosity, *args, **kwargs):
+        self.messages = messages
+        self.verbosity = verbosity
+        self.in_quotestab_div = False
+        self.in_quotestab_tr = False
+        self.in_quotestab_td = False
+        self.in_quotepc_span = False
+        self.in_quotepc_inner_span = False
+        self.quotepc_isneg = False
+        self.td_count = 0
+        self.inlasttradeprice_span = False
+        self.quote = None
+        self.quotes = []
+        HTMLParser.__init__(self, *args, **kwargs)
+    def handle_starttag(self, tag, attrs):
+        attrs = dict(attrs)
+        if tag == "div" and attrs.get("id")=="yfitp" and attrs.get("class")=="yfitabsc":
+            self.in_quotestab_div = True
+        elif tag == "tr" and self.in_quotestab_div:
+            self.in_quotestab_tr = True
+            self.td_count = 0
+        elif tag == "td" and self.in_quotestab_tr:
+            self.td_count += 1
+            self.in_quotestab_td = True
+        elif tag == "span" and self.in_quotestab_td and self.td_count == 3 and attrs.get("id").startswith("yfs_l"):
+            self.inlasttradeprice_span = True
+        elif tag == "span" and self.in_quotestab_td and self.td_count == 4 and attrs.get("id").startswith("yfs_p"):
+            self.in_quotepc_span = True
+        elif tag == "span" and self.in_quotepc_span:
+            self.in_quotepc_inner_span = True
+            self.quotepc_isneg = attrs["class"].endswith("red")
+        elif tag == "b" and self.in_quotepc_span:
+            self.in_quotepc_inner_span = True
+            self.quotepc_isneg = ("cc0000" in attrs["style"])
+    def handle_endtag(self, tag):
+        if tag == "div" and self.in_quotestab_div:
+            self.in_quotestab_div = False
+        elif tag == "tr" and self.in_quotestab_tr:
+            self.in_quotestab_tr = False
+            self.td_count = 0
+            if self.quote:
+                self.quotes.append(self.quote)
+                if self.verbosity > 5:
+                    print "%s(%s) %s, %s%%" % (self.quote.name,
+                                                self.quote.code,
+                                                unicode(self.quote.last_trade_price),
+                                                unicode(self.quote.percent))
+                                            
+                self.quote = None
+        elif tag == "td" and self.in_quotestab_td:
+            self.in_quotestab_td = False
+        elif tag == "span" and self.in_quotepc_inner_span:
+            self.in_quotepc_inner_span = False
+        elif tag == "b" and self.in_quotepc_inner_span:
+            self.in_quotepc_inner_span = False
+        elif tag == "span" and self.in_quotepc_span:
+            self.in_quotepc_span = False
+        elif tag == "span" and self.inlasttradeprice_span:
+            self.inlasttradeprice_span = False
+    def handle_data(self, data):
+        data = data.strip()
+        if data:
+            if self.in_quotestab_td:
+                if self.td_count == 1:
+                    if self.verbosity >= 5:
+                        print "\tSymbol: %s" % data
+                    self.quote = StockQuote(data)
+                elif self.td_count == 2:
+                    if self.verbosity >= 5:
+                        print "\tName: %s" % data
+                    self.quote.name = data
+                elif self.td_count == 3 and self.inlasttradeprice_span:
+                    val = decimal.Decimal(data)
+                    if self.verbosity >= 5:
+                        print "\tLast trade price: %s" % repr(val)
+                    self.quote.last_trade_price = val
+                elif self.td_count == 4 and self.in_quotepc_inner_span:
+                    val = data.strip("(%)")
+                    if self.quotepc_isneg:
+                        val = "-" + val
+                    val = decimal.Decimal(val)
+                    if self.verbosity >= 5:
+                        print "\tPercent Change: %s" % repr(val)
+                    self.quote.percent = val
+               
 def update_data(loader, verbosity=0):
     messages = []
     try:
+        if verbosity > 5:
+            print "Getting ASX-200 data..."
         messages = call_in_transaction(get_asxdata,messages, verbosity)
+        if verbosity > 2:
+            messages.append("ASX-200 data updated")
+        if verbosity > 5:
+            print "Getting Active stock quote data..."
+        messages = call_in_transaction(get_asxquotes, messages, verbosity)
     except HTMLParseError, e:
         raise LoaderException("Error parsing Yahoo! Finance website: %s" % unicode(e))
     return messages
@@ -113,3 +257,27 @@ def get_asxdata(messages, verbosity):
     parser.feed(resp.read())
     http.close()
     return parser.messages
+
+def get_asxquotes(messages, verbosity):
+    http = httplib.HTTPSConnection("au.finance.yahoo.com")
+    http.request("GET", "https://au.finance.yahoo.com/actives?e=ax")
+    resp = http.getresponse()
+    parser = ASXQuoteHtmlParser(messages, verbosity)
+    parser.feed(resp.read())
+    http.close()
+    if verbosity > 2:
+        parser.messages.append("%d stock quotes found" % len(parser.quotes))
+    if len(parser.quotes) > 0:
+        clear_statistic_list("asx", "nsw", "rt", "stock_prices")
+        sort_order = 10
+        for q in parser.quotes[0:25]:
+            add_statistic_list_item("asx", "nsw", "rt", "stock_prices",
+                    q.last_trade_price, sort_order,
+                    label = q.code,
+                    trend = q.trend())
+            sort_order += 10
+    if verbosity > 1:
+        parser.messages.append("%d stock quotes saved" % (len(parser.quotes[0:25])))
+    return parser.messages
+
+ 
