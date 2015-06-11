9a70f433cca8bd479696ba6f29b335caaa0d2ee7
Paul Haesler <paul.haesler@nicta.com.au>
2015-06-12 09:37:33 +1000

    s/year/years/ - event list editer broken.

--- dashboard_loader/dashboard_loader/dynform.py
+++ dashboard_loader/dashboard_loader/dynform.py
@@ -34,7 +34,7 @@ def get_form_class_for_statistic(stat):
         form_fields["label"] = forms.CharField(required=True, max_length=120)
         field_count += 1
     elif stat.is_eventlist():
-        form_fields["date"] = forms.DateField(required=False, widget=SelectDateWidget(year=range(2000, datetime.date.today().year+5)))
+        form_fields["date"] = forms.DateField(required=False, widget=SelectDateWidget(years=range(2000, datetime.date.today().year+5)))
         def clean_check_date(self, data):
             if not data["date"]:
                 self.add_error("date", "This field is required")
