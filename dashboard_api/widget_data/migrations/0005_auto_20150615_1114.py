3a0d7e6b0e1fc9eb21d1cff5eb5aa8ae9394db2e
Paul Haesler <paul.haesler@nicta.com.au>
2015-06-15 14:59:34 +1000

    timezone aware migration procedure.

--- dashboard_api/widget_data/migrations/0005_auto_20150615_1114.py
+++ dashboard_api/widget_data/migrations/0005_auto_20150615_1114.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import pytz
 
+from django.conf import settings
 from django.db import models, migrations
 
 def migrate_datekeys(apps, schema_editor):
+    tz = pytz.timezone(settings.TIME_ZONE)
     StatisticListItem = apps.get_model("widget_data", "StatisticListItem")
     for si in StatisticListItem.objects.filter(datekey__isnull=False):
-        si.datetime_key = si.datekey
+        si.datetime_key = tz.localize(si.datekey)
         si.save()
 
 class Migration(migrations.Migration):
